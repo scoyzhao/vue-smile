29e73dfc8469671d7abc20a1446b5003ccdac938
scoyzhao <scoyzhao@163.com>
2018-05-23 15:13:20 +0800

    auther信息

--- src/filter/moneyFilter.js
+++ src/filter/moneyFilter.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: scoyzhao 
+ * @Date: 2018-05-23 15:13:07 
+ * @Last Modified by:   scoyzhao 
+ * @Last Modified time: 2018-05-23 15:13:07 
+ */
+
 export function toMoney(money = 0) {
     return money.toFixed(2)
 }
