8606894ada61a2b4d1b2afaba9d27192d44cad67
scoyzhao <scoyzhao@163.com>
2018-06-14 10:08:02 +0800

    前端表单检测

--- src/components/pages/Register.vue
+++ src/components/pages/Register.vue
@@ -14,6 +14,7 @@
                 placeholder="请输入用户名"
                 required
                 @click-icon="username = ''"
+                :error-message="usernameErrorMsg"
             />
             <van-field 
                 v-model="password"
@@ -21,9 +22,10 @@
                 label="密码"
                 placeholder="请输入密码"
                 required
+                :error-message="passwordErrorMsg"
             />
             <div class="register-button">
-                <van-button type="primary"  @click="registerUser"  :loading="openLoading" size="large">马上注册</van-button>
+                <van-button type="primary"  @click="registerAction"  :loading="openLoading" size="large">马上注册</van-button>
             </div>
         </div>
     </div>
@@ -39,16 +41,19 @@ export default {
       username: "",
       password: "",
       // 是否开启用户的Loading
-      openLoading: false
+      openLoading: false,
+      usernameErrorMsg: "",
+      passwordErrorMsg: ""
     };
   },
   methods: {
     goBack() {
       this.$router.go(-1);
     },
+    //用户注册
     registerUser() {
       // 进入这个方法的时候，先让他loading，防止重复提交
-      this.openLoading = true
+      this.openLoading = true;
       axios({
         url: this.$url.registerUser,
         method: "post",
@@ -70,6 +75,30 @@ export default {
           Toast.fail("注册失败");
           this.openLoading = false;
         });
+    },
+    // 表单检测
+    checkForm() {
+      let isOk = true;
+
+      if (this.username.length < 5) {
+        this.usernameErrorMsg = "用户名不能小于5位";
+        isOk = false;
+      } else {
+        this.usernameErrorMsg = "";
+      }
+
+      if (this.password.length < 6) {
+        this.passwordErrorMsg = "密码不能小于6位";
+        isOk = false;
+      } else {
+        this.passwordErrorMsg = "";
+      }
+
+      return isOk;
+    },
+    // 完整注册逻辑
+    registerAction() {
+      this.checkForm() && this.axiosRegisterUser();
     }
   }
 };
